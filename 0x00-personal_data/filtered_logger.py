515e66b0839e502e8dacd90f3fc8632cf0cb92ae
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-16 08:12:49 +0100

    Fixed pycodestyle error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,6 +17,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]) -> None:
+        """Initialization"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
