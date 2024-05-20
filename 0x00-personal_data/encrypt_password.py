6a8d6e83b07f3d5f66f81c9c9be44b92be311006
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-20 14:18:44 +0100

    Checking valid hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,13 @@ def hash_password(password: str) -> bytes:
     password = password.encode("utf-8")
     hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hash_password: bytes, password: str) -> bool:
+    """
+    Validate the provided password matches the hashed password
+    """
+    password = password.encode("utf-8")
+    if bcrypt.checkpw(password, hash_password):
+        return True
+    return False
