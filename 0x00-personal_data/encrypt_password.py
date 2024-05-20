a4ca4be62d4983774166d7a01a04a2334bc49c52
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-20 14:08:50 +0100

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A module that hashes a password
+"""
+import bcrypt
+
+
+def hash_password(password: str):
+    """
+    Hashes a string into a password
+    """
+    password = password.encode("utf-8")
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed_password
