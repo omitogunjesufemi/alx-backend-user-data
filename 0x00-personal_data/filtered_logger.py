f9507ff2afa887639dbd0f28c7939d4fba692727
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-20 13:31:17 +0100

    Annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,7 +21,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """Initialization"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
