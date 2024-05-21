86d12c20ede6595900e8e357f438a4338e9445ae
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-21 13:54:33 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauth() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - execute the 401 handler
+    """
+    abort(401)
