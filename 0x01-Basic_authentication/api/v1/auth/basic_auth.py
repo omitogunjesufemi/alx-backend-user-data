03625ff38032cb6f6d49589be772bc5a1a653744
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-21 15:49:52 +0100

    Returning the Base64 part of the Authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,20 @@ class BasicAuth(Auth):
     """
     BasicAuth class inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) != str:
+            return None
+
+        starts_with_basic = authorization_header.startswith("Basic ")
+        if starts_with_basic:
+            return authorization_header.split("Basic ")[1]
+        else:
+            return None
