f0e20bc781053d22b4036e80b4d0a3c2d9fe3751
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-21 14:08:49 +0100

    Class template for the authentication system to be implemented

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+This module contains the Auth class
+"""
+from flask import request
+from Typing import List
+
+
+class Auth:
+    """
+    Class Auth: template for all authentication system to be implemented
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Return: False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Return: None
+
+        request - Flask request object
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Return: None
+
+        request - Flask request object
+        """
+        return None
