ad9a165fbe5370d420cf6f782a05a90500028928
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-21 22:10:52 +0100

    Base64 decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 This module contains the BasicAuth class
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,4 +26,22 @@ class BasicAuth(Auth):
         if starts_with_basic:
             return authorization_header.split("Basic ")[1]
         else:
+            return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string:
+        - base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if type(base64_authorization_header) != str:
+            return None
+
+        try:
+            decoded_value = base64.b64decode(base64_authorization_header)
+            decoded_value = decoded_value.decode("utf-8")
+            return decoded_value
+        except:
             return None
