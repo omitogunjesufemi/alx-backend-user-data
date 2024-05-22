7191235b2987d0cef55cbaed4ce29fd800b2209a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-22 22:51:37 +0100

    Correct wrong intialization of attribute

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,26 @@
 This module contains the SessionAuth class that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Class SessionAuth that inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for user_id
+        """
+        if user_id is None:
+            return None
+
+        if type(user_id) != str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
