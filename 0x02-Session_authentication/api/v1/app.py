5ed3208ae2111610f64becdbb896ae5e1bd7bbee
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-22 12:44:34 +0100

    Created SessionAuth class, another type of authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,7 +18,10 @@ env_auth = getenv("AUTH_TYPE")
 if env_auth:
     auth = env_auth
 
-if auth == "basic_auth":
+if auth == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif auth == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the SessionAuth class that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Class SessionAuth that inherits from Auth
+    """
+    pass
