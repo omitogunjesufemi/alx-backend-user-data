6a19b97171281d4d78e9959fd55d6e34008d3d75
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-28 13:27:23 +0100

    Implement profile that shows the user email using the session id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,20 @@ def logout():
     else:
         abort(403)
 
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """Return details of user"""
+    try:
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": f"{user.email}"}), 200
+        else:
+            abort(403)
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
