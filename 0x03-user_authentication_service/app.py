88cfbd33d5dfb7ba47ec67f06f57f92f1682283a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-28 11:58:39 +0100

    Register user using authentication system written

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,16 +2,31 @@
 """
 Flask App
 """
-from flask import jsonify, Flask
+from flask import jsonify, Flask, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     """ Set up"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """End point to register user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": f"{user.email}",
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
