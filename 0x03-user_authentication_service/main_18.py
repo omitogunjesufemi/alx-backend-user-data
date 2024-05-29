7b0e551df7ee3ec9178777682c82d93b4b43164a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-29 11:06:00 +0100

    Update update_password method

--- 0x03-user_authentication_service/main_18.py
+++ 0x03-user_authentication_service/main_18.py
@@ -10,10 +10,16 @@ auth = Auth()
 
 auth.register_user(email, password)
 
-print(auth.create_session(email))
+session_id = auth.create_session(email)
+print(session_id)
 print(auth.create_session("unknown@email.com"))
+
+user = auth.get_user_from_session_id(session_id)
+print(f"User: {user.email}, {user.hashed_password}")
 
 reset_token = auth.get_rest_password_token(email)
 print(f"Reset Token: {reset_token}")
 
 auth.update_password(reset_token, "Jesus loves me")
+user = auth.get_user_from_session_id(session_id)
+print(f"User: {user.email}, {user.hashed_password}")
