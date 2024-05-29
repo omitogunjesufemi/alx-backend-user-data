d5aacf9180868ceadccacd53409609abe0da6ae5
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-29 10:39:35 +0100

    Update get_reset_password_token method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,7 +87,9 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
-        except Exception:
+        except NoResultFound:
+            raise ValueError
+        except InvalidRequestError:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
